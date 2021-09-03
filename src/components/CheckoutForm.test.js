8c2e35b5e99a897e3c04e28e3d29dea4c13f2a5f
sihan sun <haniyya2@gmail.com>
2021-09-03 12:55:56 -0700

    new change

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import MutationObserver from 'mutationobserver-shim';
-import { render } from "@testing-library/react";
+import { render , screen} from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 import userEvent from "@testing-library/user-event";
 
