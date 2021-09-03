2916311aa44fd3498310ab6cb01a17f7037dd882
sihan sun <haniyya2@gmail.com>
2021-09-03 12:53:37 -0700

    new change

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -2,6 +2,7 @@ import React from "react";
 import MutationObserver from 'mutationobserver-shim';
 import { render } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
+import userEvent from "@testing-library/user-event";
 
 
 // Write up the two tests here and make sure they are testing what the title shows
@@ -24,4 +25,22 @@ test("renders without errors", () => {
     render(<CheckoutForm />)
 });
 
-test("shows success message on submit with form details", () => {});
+test("shows success message on submit with form details", () => {
+    render(<CheckoutForm />)
+    const firstNameInput = screen.getByLabelText(/First Name:/i)
+    userEvent.type(firstNameInput, "sihan")
+    const lastNameInput = screen.getByLabelText(/Last Name:/i)
+    userEvent.type(lastNameInput, "sun")
+    const addressInput = screen.getByLabelText(/Address:/i)
+    userEvent.type(addressInput, "6301 SHELLMOUND ST, APT 417")
+    const cityInput = screen.getByLabelText(/City:/i)
+    userEvent.type(cityInput, "emeryville")
+    const stateInput = screen.getByLabelText(/State:/i)
+    userEvent.type(stateInput, 'ca')
+    const zipInput = screen.getByLabelText(/Zip:/i)
+    userEvent.type(zipInput, "94608")
+    const button = screen.getByRole("button")
+    userEvent.click(button)
+    const feedback = screen.queryByText(/sihan/i)
+    expect(feedback).toBeInTheDocument()
+});
